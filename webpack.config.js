ba549d20e85d957b95de24f43320ac764ad9c344
BestVersion7 <hunterkfan@gmail.com>
2021-11-23 22:12:18 -0500

    changed server

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,18 @@
 // webpack.config.js
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  target: 'node',
-  plugins: [
-    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/})
-  ],
-  module: {
-    rules: [
-      {
-        loader: 'ts-loader',
-        options: {
-        transpileOnly: true
-        },
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx']
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: 'handler.js'
-  },
-  mode: 'development'
+    target: "node",
+    plugins: [new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })],
+    module: {
+        rules: [
+            {
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true,
+                },
+            },
+        ],
+    }
 };
