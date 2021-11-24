6c8e9692691698012ab3dc4bb287aa11ab722e9c
BestVersion7 <hunterkfan@gmail.com>
2021-11-23 21:49:00 -0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
+// webpack.config.js
+const path = require('path');
 const webpack = require('webpack');
 
-const webpackConfig = {
-  plugins: [
-    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
-  ]
-}
-
 module.exports = {
-    webpackConfig
-}
+  target: 'node',
+  plugins: [
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/})
+  ],
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        options: {
+        transpileOnly: true
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx']
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: 'handler.js'
+  },
+  mode: 'development'
+};
